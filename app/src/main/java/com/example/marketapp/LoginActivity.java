b9772e27deed9ce61622355733e10a134ffdd25d
Gihoon Kim <gkim61@myseneca.ca>
2021-03-23 22:01:22 -0400

    commit

---
+++ app/src/main/java/com/example/marketapp/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.marketapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextID)
+    EditText editTextID;
+    @BindView(R.id.editTextPassword)
+    EditText editTextPassword;
+    @BindView(R.id.buttonLogin)
+    Button buttonLogin;
+
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginactivity);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonLogin)
+    public void onLoginButtonClicked() {
+
+    }
+}
---
+++ app/src/main/java/com/example/marketapp/RegisterActivity.java
@@ -0,0 +1,6 @@
+package com.example.marketapp;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
