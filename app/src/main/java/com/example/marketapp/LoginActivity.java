e7d22ea6c0ec84c423109368add7e9c0f5731e34
Gihoon Kim <gkim61@myseneca.ca>
2021-03-26 21:48:23 -0400

    except Build.Gradle

--- app/src/main/java/com/example/marketapp/LoginActivity.java
+++ app/src/main/java/com/example/marketapp/LoginActivity.java
@@ -1,17 +1,22 @@
 package com.example.marketapp;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    private static final String TAG = "LoginActivity";
 
     @BindView(R.id.editTextID)
     EditText editTextID;
@@ -19,8 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText editTextPassword;
     @BindView(R.id.buttonLogin)
     Button buttonLogin;
-
-    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +32,44 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.loginactivity);
 
         ButterKnife.bind(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @OnClick(R.id.buttonLogin)
     public void onLoginButtonClicked() {
 
-        // Add
-        // Add
-        // for Bit Bucket
+        signIn();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void signIn() {
+
+        String email = editTextID.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+
+                    if (task.isSuccessful()) {
+
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                    } else {
+
+                        // If sign in fails, display a message to the user
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/marketapp/RegisterActivity.java
+++ app/src/main/java/com/example/marketapp/RegisterActivity.java
@@ -1,6 +1,94 @@
 package com.example.marketapp;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends Activity {
+
+    private static final String TAG = "RegisterActivity";
+
+    @BindView(R.id.editTextEmail)
+    EditText editTextEmail;
+    @BindView(R.id.editTextPassword)
+    EditText editTextPassword;
+    @BindView(R.id.editTextPasswordCheck)
+    EditText editTextPasswordCheck;
+    @BindView(R.id.buttonRegister)
+    Button buttonRegister;
+
+    private FirebaseAuth mAuth;
+    private String email = "";
+    private String password = "";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registeractivity);
+
+        ButterKnife.bind(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @OnClick(R.id.buttonRegister)
+    public void onRegisterButton() {
+
+        if (!editTextEmail.getText().toString().equals("")) {
+
+            email = editTextEmail.getText().toString();
+        }
+
+        if (editTextPassword.getText().toString().equals(editTextPasswordCheck.getText().toString())) {
+
+            password = editTextPasswordCheck.getText().toString();
+        }
+    }
+
+    private void signUp() {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:Success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+
+                            // If sign in fails, display a message to the user
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
